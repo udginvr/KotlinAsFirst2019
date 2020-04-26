fb07837e4f9e58fdfb903358f5a1220277a26457
Udginvr <udginvr@rambler.ru>
2020-04-26 21:21:38 +0300

    test

--- src/my.kt
+++ src/my.kt
@@ -7,6 +7,6 @@ fun multiplyAll(list: List<Int>) = list.fold(1.0) { zzz, previousResult ->
  */
 fun main() {
 //    println(multiplyAll(listOf(3, 4, 5)))
-    val tstamp = 62703026 / 1000
+    val tstamp = 62703028 / 1000
     println("${tstamp / 3600}:${tstamp % 3600 / 60}:${tstamp % 60}")
 }
